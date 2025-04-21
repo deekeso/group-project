02b6c10d88fdf47c04487df0e3ca0ce363306457
PLUS\Coleen.Garcia <Coleen.Garcia@tbu.net>
2025-04-21 13:20:03 +0800

    merge: highlight cell for number of matches from feature/pay-table

--- src/components/PayTable/PayTable.vue
+++ src/components/PayTable/PayTable.vue
@@ -1,31 +1,35 @@
 <script lang="ts" setup>
-  import payTable from "./payTable.json"
-  const { selectedCellsCount, matchedCellsCount } = defineProps<{
-    selectedCellsCount: number
-    matchedCellsCount: number
-  }>()
+import payTable from './payTable.json'
+const { selectedCellsCount, matchedCellsCount } = defineProps<{
+  selectedCellsCount: number
+  matchedCellsCount: number
+}>()
 </script>
 
 <template>
   <div v-if="selectedCellsCount > 0" class="pay-table">
     <div class="cells-container">
       <div v-for="n in selectedCellsCount" class="pay-data">
-        <div v-if="n <= 6" class="multiplier-cell-tight" :class="{ hit: matchedCellsCount >= n}">
-          <el-text size="large">{{ payTable['mini'][selectedCellsCount - 1][n-1] }}x</el-text>
+        <div v-if="n <= 6" class="multiplier-cell-tight" :class="{ hit: matchedCellsCount >= n }">
+          <el-text size="large">{{ payTable['mini'][selectedCellsCount - 1][n - 1] }}x</el-text>
         </div>
-        <div v-else class="multiplier-cell" :class="{ hit: matchedCellsCount >= n}">
-          <el-text size="large">{{ payTable['mini'][selectedCellsCount - 1][n-1] }}x</el-text>
+        <div v-else class="multiplier-cell" :class="{ hit: matchedCellsCount >= n }">
+          <el-text size="large">{{ payTable['mini'][selectedCellsCount - 1][n - 1] }}x</el-text>
         </div>
-        <div v-if="n <= 6" class="selected-count-cell-tight" :class="{ hit: matchedCellsCount >= n}">
+        <div
+          v-if="n <= 6"
+          class="selected-count-cell-tight"
+          :class="{ hit: matchedCellsCount >= n }"
+        >
           <el-text size="large">{{ n }}x</el-text>
         </div>
-        <div v-else class="selected-count-cell" :class="{ hit: matchedCellsCount >= n}">
+        <div v-else class="selected-count-cell" :class="{ hit: matchedCellsCount >= n }">
           <el-text size="large">{{ n }}x</el-text>
         </div>
       </div>
     </div>
   </div>
-  
+
   <div v-else class="pay-table">
     <div class="cells-container">
       <div class="pay-data">
@@ -41,69 +45,71 @@
 </template>
 
 <style scoped>
-  .pay-table {
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    font-weight: bold;
-  }
+.pay-table {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-weight: bold;
+}
 
-  .cells-container {
-    width: 100%;
-    display: flex;
-    gap: 4px;
-    justify-content: space-between;
-    align-items: center;
-  }
-  
-  .pay-data {
-    width: 100%;
-    height: fit-content;
-    display: flex;
-    justify-content: space-between;
-    align-items: stretch;
-    flex-direction: column;
-    gap: 10px;
-  }
+.cells-container {
+  width: 100%;
+  display: flex;
+  gap: 4px;
+  justify-content: space-between;
+  align-items: center;
+}
 
-  .multiplier-cell, .multiplier-cell-tight {
-    background-color: #524DE0;
-  }
-  
-  .selected-count-cell, .selected-count-cell-tight {
-    background-color: #964DE0;
-  }
+.pay-data {
+  width: 100%;
+  height: fit-content;
+  display: flex;
+  justify-content: space-between;
+  align-items: stretch;
+  flex-direction: column;
+  gap: 10px;
+}
 
-  .el-text {
-    color: white;
-    font-weight: 600;
-  }
+.multiplier-cell,
+.multiplier-cell-tight {
+  background-color: #524de0;
+}
 
-  .multiplier-cell, 
-  .selected-count-cell, 
-  .multiplier-cell-tight, 
-  .selected-count-cell-tight, 
-  .placeholder-cell {
-    width: 100%;
-    padding: 4px 10px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border-radius: 8px;
-    border: 2px solid rgba(0,0,0,0);
-  }
+.selected-count-cell,
+.selected-count-cell-tight {
+  background-color: #964de0;
+}
 
-  .hit {
-    border: 2px solid greenyellow;
-  }
+.el-text {
+  color: white;
+  font-weight: 600;
+}
 
-  .multiplier-cell-tight, .selected-count-cell-tight {
-    padding: 4px 0;
-  }
+.multiplier-cell,
+.selected-count-cell,
+.multiplier-cell-tight,
+.selected-count-cell-tight,
+.placeholder-cell {
+  width: 100%;
+  padding: 4px 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 8px;
+  border: 2px solid rgba(0, 0, 0, 0);
+}
 
-  .placeholder-cell {
-    background-color: #7674a7;
-  }
+.hit {
+  border: 2px solid greenyellow;
+}
 
+.multiplier-cell-tight,
+.selected-count-cell-tight {
+  padding: 4px 0;
+}
+
+.placeholder-cell {
+  background-color: #7674a7;
+}
 </style>
