c40cd6d33000f9511e6e19db42e690533c352767
Sebastian Martinez <Sebastian.martinez@tbu.net>
2025-04-24 13:18:29 +0800

    refactor: indent GameButtons script and style

--- src/components/GameButtons.vue
+++ src/components/GameButtons.vue
@@ -22,66 +22,66 @@
 </template>
 
 <script setup lang="ts">
-import { useGameStore } from '@/stores/useGameStore'
-import { storeToRefs } from 'pinia'
+  import { useGameStore } from '@/stores/useGameStore'
+  import { storeToRefs } from 'pinia'
 
-const gameStore = useGameStore()
-const { wager, bet } = storeToRefs(gameStore)
+  const gameStore = useGameStore()
+  const { wager, bet } = storeToRefs(gameStore)
 </script>
 
 <style scoped>
-.btn-container {
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  margin-block: 10px;
-  gap: 5px;
-}
-.btn {
-  font-size: 1rem;
-  text-align: center;
-  font-weight: bold;
-  border-radius: 100px;
-  padding-block: 0.5rem;
-}
+  .btn-container {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    margin-block: 10px;
+    gap: 5px;
+  }
+  .btn {
+    font-size: 1rem;
+    text-align: center;
+    font-weight: bold;
+    border-radius: 100px;
+    padding-block: 0.5rem;
+  }
 
-.yellow-btn {
-  background: #f8ab00;
-  color: #050505;
-  border: 4px solid #ffe387;
-  padding-inline: 20px;
-}
-.wager-container {
-  background: #f8ab00;
-  color: #050505;
-  font-weight: bolder;
-  border-radius: 100px;
-  flex: 1;
-  padding-block: 0;
+  .yellow-btn {
+    background: #f8ab00;
+    color: #050505;
+    border: 4px solid #ffe387;
+    padding-inline: 20px;
+  }
+  .wager-container {
+    background: #f8ab00;
+    color: #050505;
+    font-weight: bolder;
+    border-radius: 100px;
+    flex: 1;
+    padding-block: 0;
 
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  overflow: hidden;
-}
-.wager-btn {
-  background: #f8ab00;
-  border: none;
-  padding-inline: 20px;
-  height: 100%;
-  font-weight: bold;
-  align-content: center;
-}
-.wager-btn:hover {
-  background: #ffb619;
-}
-.wager-btn,
-.wager-txt {
-  font-size: 1.2rem;
-  font-weight: 550;
-}
-.wager-icon {
-  fill: black;
-  height: 1.2rem;
-}
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    overflow: hidden;
+  }
+  .wager-btn {
+    background: #f8ab00;
+    border: none;
+    padding-inline: 20px;
+    height: 100%;
+    font-weight: bold;
+    align-content: center;
+  }
+  .wager-btn:hover {
+    background: #ffb619;
+  }
+  .wager-btn,
+  .wager-txt {
+    font-size: 1.2rem;
+    font-weight: 550;
+  }
+  .wager-icon {
+    fill: black;
+    height: 1.2rem;
+  }
 </style>
