dc16892aea30b8aae45f91b36e0fc4d05852556e
PLUS\Joana.Borigas <joana.borigas@tbu.net>
2025-05-02 17:23:55 +0800

    amount radio button auto deselect if custom input is used

--- src/views/TheWallet.vue
+++ src/views/TheWallet.vue
@@ -125,6 +125,14 @@ watch(radio2, (val) => {
   }
 })
 
+watch(num, (val) => {
+  const predefinedAmounts = ['20', '50', '100', '200', '500', '1000']
+  if (!predefinedAmounts.includes(String(val))) {
+    radio2.value = ''
+  }
+})
+
+
 function confirmDeposit() {
   if (!value.value) {
     ElMessage.error('Please select a payment method.')
