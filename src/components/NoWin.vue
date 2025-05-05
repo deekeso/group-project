e0629af9b3cb6806fbba42736fda92c9fa9df916
PLUS\Joana.Borigas <joana.borigas@tbu.net>
2025-05-05 12:45:58 +0800

    removed lose sound effect

--- src/components/NoWin.vue
+++ src/components/NoWin.vue
@@ -4,20 +4,10 @@
 
 <script setup lang="ts">
 import { ElMessage } from 'element-plus'
-import { onMounted } from 'vue'
-import failSound from '@/assets/sounds/nowin/8-bit-video-game-fail-version-2-145478.mp3'
 
-const playLoseSound = () => {
-  const audio = new Audio(failSound)
-  audio.play()
-}
-
-onMounted(() => {
-  playLoseSound()
-  ElMessage({
-    message: "No luck this round. Let's go again!",
-    type: 'error',
-    plain: true,
-  })
+ElMessage({
+  message: "No luck this round. Let's go again!",
+  type: 'error',
+  plain: true,
 })
 </script>
