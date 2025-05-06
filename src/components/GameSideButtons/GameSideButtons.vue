dd07ef6bd99db32a3aa5ce344e780de5aa2636d6
Coleen . <coleengarciado@gmail.com>
2025-05-06 14:41:55 +0800

    something

--- src/components/GameSideButtons/GameSideButtons.vue
+++ src/components/GameSideButtons/GameSideButtons.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="side-buttons">
-    <AutopickButton @numberSelected="(number: number) => emit('numberSelected', number)" :max-number />
+    <AutopickButton
+      @numberSelected="(number: number) => emit('numberSelected', number)"
+      :max-number="maxNumber"
+      :key="currentIndex"
+    />
     <ClearButton class="clear-btn" @click="$emit('clear')" />
   </div>
 </template>
@@ -11,20 +15,20 @@ import ClearButton from './ClearButton.vue'
 
 const { maxNumber } = defineProps<{
   maxNumber: number
+  currentIndex: number
 }>()
 
 const emit = defineEmits<{
   (e: 'numberSelected', number: number): void
   (e: 'clear'): void
 }>()
-
 </script>
 
 <style scoped>
 .side-buttons {
   display: flex;
   flex-direction: column;
-  gap: 5px ;
+  gap: 5px;
 }
 .clear-btn {
   flex: 1;
