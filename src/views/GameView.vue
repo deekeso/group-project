3d245ad392a2311fbbbf8a2a6b5610c8ca6203cb
PLUS\Aidan.Rogado <aidan.rogado@tbu.net>
2025-05-07 14:09:00 +0800

    hotfix: Wager is deducted at the start of the draw

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -109,13 +109,15 @@ import PurchaseCard from '@/components/PurchaseCard.vue'
 
 import { useTour } from '@/composables/useTour'
 
+import { TransactionOperation } from '@/types'
+
 const { open } = useTour()
 
 const router = useRouter()
 const route = useRoute()
 const gameType: GameType = route.meta.gameType as GameType
 const gameStore = useGameStore()
-const { wallet } = useAuthStore()
+const { wallet, performTransaction } = useAuthStore()
 const { matchedNumbers, selectedNumbers, winnings, result, hasPurchasedCards } =
   storeToRefs(gameStore)
 const { classicKenoDraw, miniKenoDraw, kenoAutopick, resetDraw, resetAutopicked } = useKenoDraw()
@@ -178,6 +180,8 @@ async function startDraw() {
     })
     return
   }
+
+  performTransaction(TransactionOperation.Wage, gameStore.wager)
 
   resetDraw()
   displayMatching.value = true
