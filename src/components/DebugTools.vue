398ce568b04da4ab7b5a57ae888ae66cf6a3e684
PLUS\Aidan.Rogado <aidan.rogado@tbu.net>
2025-05-07 18:08:58 +0800

    W.I.P roulette

--- src/components/DebugTools.vue
+++ src/components/DebugTools.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="debug-tools">
     <button @click="trigger20Losses">Trigger 20 losses</button>
+    <BonusSpin />
   </div>
 </template>
 
 <script setup lang="ts">
-  import { useGameStore } from '@/stores/useGameStore';
-  const { setLostStreak, setResult } = useGameStore()
-  function trigger20Losses() {
-    setLostStreak(19)
-    setResult('lose')
-  }
+import { useGameStore } from '@/stores/useGameStore'
+import BonusSpin from './BonusSpin.vue'
+const { setLostStreak, setResult } = useGameStore()
+function trigger20Losses() {
+  setLostStreak(19)
+  setResult('lose')
+}
 </script>
 
 <style scoped>
